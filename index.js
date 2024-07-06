cc79495e71deb23611d3ca0fe0209df506dbafc7
S-D123 <shrimandasadiya125@gmail.com>
2024-07-06 10:07:01 +0530

    Project Timestamp

--- index.js
+++ index.js
@@ -24,7 +24,43 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", (req, res) => {
+  a = new Date();
+  b = a.toUTCString()
+  c = Date.parse(b)
+  res.json({"unix": c, "utc": b});
+})
 
+app.get("/api/:date", (req, res) => {
+  let DATE = req.params.date; // it is already converted to a string
+  // The problem is timestamp cannot be converted to date
+  if(isNaN(DATE)){ // Not a number = true => string => of type 2015-25-12
+    
+    // DATE = DATE.endsWith('Z') ? DATE : DATE + 'Z';
+    // new Date = (UTC Date + Local time zone)Time (GMT+TimeZone)Details
+    // it correctly interprets current Date, but if any other date is written
+    // new Date.toUTCStr = Input Date - Local Time
+
+    // if Z is written at end, then it tell the server to return only UTC date
+    // it tells the server that it is already calibrated to UTC
+
+    // what happens is that "new Date()" always considers/incl local time zone
+    // when that time is converted to "toUTCString()", then it subtracts
+    // local time zone => suppose "new Date().toUTCString()" gives 31 Dec 1969
+     
+    // but when we write Z at end of date, it tells him that it is already 
+    // utc time. so it doesn't subtract anything
+
+    // convert it to date that is global time
+    a = new Date(DATE).toUTCString();
+  } else { // if its a number, then its a time stamp
+    a = new Date(parseInt(DATE)).toUTCString();
+  }
+
+  if (a == "Invalid Date") {res.json({error: "Invalid Date"})}
+  b = Date.parse(a);
+  res.json({"unix": b, "utc": a});
+})
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
